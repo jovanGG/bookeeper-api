951547d2657968abec9b06c7ec4ab73c52379c38
Jovan <37410787+jovanGG@users.noreply.github.com>
2023-01-06 17:36:50 +0100

    Added orm, docker, updated services, config

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,7 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { Book } from './entities/books.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
+import { Book } from './entities/book.entity';
+import { CreateBookDto, UpdateBookDto } from './dto';
 
 @Injectable()
 export class BooksService {
-  private books: Book[] = [];
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepository: Repository<Book>,
+  ) {}
+
+  findAll() {
+    return this.bookRepository.find();
+  }
+
+  async findOne(id: string) {
+    const book = await this.bookRepository.findOne({ where: { id: +id } });
+
+    if (!book) {
+      throw new NotFoundException(`Book #${id} doesn't seem to exist.`);
+    }
+
+    return book;
+  }
+
+  create(createBookDto: CreateBookDto) {
+    const book = this.bookRepository.create(createBookDto);
+
+    return this.bookRepository.save(book);
+  }
+
+  async update(id: string, updateBookDto: UpdateBookDto) {
+    const book = await this.bookRepository.preload({
+      id: +id,
+      ...updateBookDto,
+    });
+
+    if (!book) {
+      throw new NotFoundException(`Book #${id} doesn't seem to exist.`);
+    }
+
+    return this.bookRepository.save(book);
+  }
+
+  async remove(id: string) {
+    const book = await this.findOne(id);
+
+    return this.bookRepository.remove(book);
+  }
 }
---
+++ src/books/entities/book.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column()
+  description: string;
+
+  @Column('json', { nullable: true })
+  specification: string[];
+}
